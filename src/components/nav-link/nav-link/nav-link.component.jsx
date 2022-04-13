de3a8ccff0955df67fb4e62d6a40351ec2cc383d
Kacper Hemperek <kacper.hemperek@o2.pl>
2022-04-13 23:14:01 +0200

    changing navlink text size

--- src/components/nav-link/nav-link/nav-link.component.jsx
+++ src/components/nav-link/nav-link/nav-link.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavLink = ({ children }) => {
   return (
-    <div className="text-center text-2xl align-middle h-16 p-4 text-xl font-semibold border-b-2 hover:text-2xl hover:cursor-pointer hover:bg-neutral-800 hover:text-neutral-50  transition-all ">
+    <div className="text-center align-middle h-16 p-4 text-xl font-semibold border-b-2 hover:text-2xl hover:cursor-pointer hover:bg-neutral-800 hover:text-neutral-50  transition-all ">
       {children}
     </div>
   );
